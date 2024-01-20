a4ab28186f86e795961c006d9115a8207f7b47ae
Michael Wakeling <developerwakeling@gmail.com>
2024-01-19 22:59:29 -0500

    Added mongodb connection and connecting via environment variable

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const port = 8080;
 const question = require('./routes/question.js');
 const user = require('./routes/user.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(express.json());
 app.use('/question', question);
 app.use('/user', user);
 
+mongoose.connect(process.env.CONNECTIONSTRING).catch((err) => console.log(err));
 // [] Authentication
 // [] Database
 // [] Logging ?
